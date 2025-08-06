f7ab7ba1a66aea76e3b747c4d7857848c2a6784c
Maxime <pro.maximebreton@gmail.com>
2025-08-06 13:46:31 +0200

    -

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -2,7 +2,7 @@ import { stretch } from "../../fonts"
 
 export default function Footer() {
   return (
-    <footer className="flex justify-end mt-6">
+    <footer className="flex justify-end mt-3">
       <p className={`text-xs ${stretch.className}`}>
         © {new Date().getFullYear()}
       </p>
