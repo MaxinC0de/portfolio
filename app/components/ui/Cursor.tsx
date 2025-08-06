e1805ac19ae90eaa4baa517dc28e06d17f71bf97
Maxime <pro.maximebreton@gmail.com>
2025-08-06 12:50:58 +0200

    -

--- app/components/ui/Cursor.tsx
+++ app/components/ui/Cursor.tsx
@@ -1,20 +1,19 @@
 "use client"
 
-import { useIsMobile } from "@/app/hooks/useIsMobile"
 import AnimatedCursor from "react-animated-cursor"
 
 export default function CustomCursor() {
-  const { isMobile } = useIsMobile()
-  if (isMobile) return null
   return (
-    <AnimatedCursor
-      innerSize={0}
-      outerSize={40}
-      color="60, 60, 60"
-      outerAlpha={0.7}
-      innerScale={0}
-      outerScale={1}
-      trailingSpeed={10}
-    />
+    <div className="hidden md:block">
+      <AnimatedCursor
+        innerSize={0}
+        outerSize={40}
+        color="60, 60, 60"
+        outerAlpha={0.7}
+        innerScale={0}
+        outerScale={1}
+        trailingSpeed={10}
+      />
+    </div>
   )
 }
