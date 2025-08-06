ae78f756c74d230a00e186bed2cf2d2bc2303fd6
Maxime <pro.maximebreton@gmail.com>
2025-08-06 15:36:37 +0200

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     locale: "fr_FR",
-    url: "https://www.studio-regenade.com",
+    url: "https://www.studioregenade.com",
     siteName: "Studio Regenade",
     images: [
       {
